b1eda61b97325a77bef3cd2f4987d7be6871e12b
James L Parry <jim_parry@bcit.ca>
2015-01-08 12:13:21 -0800

    Morning's work
    Fix a bunch of little things and start on assignment 1.
    Signed-off-by:James L Parry <jim_parry@bcit.ca>

--- application/views/show/_congrats.php
+++ application/views/show/_congrats.php
@@ -9,4 +9,10 @@
     <p>If you would take a minute to 
 	<a href="/feedback/survey/{category}/{name}">provide some feedback</a>,
 	we would appreciate it!</p>
+    <p>You can use the browser's back button to take you out of this lesson,
+	or jump directly to the Course 
+	<a href="/">home page</a>,
+	<a href="/organizer">organizer</a>, or
+	<a href="/reference">reference</a>
+    </p>
 </div>
